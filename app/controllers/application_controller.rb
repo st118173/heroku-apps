4abc30df429504ea83f6ba153b6a6333adbd59f5
st118173 <st118173@ait.asia>
2016-11-18 16:48:29 +0700

    jhv

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,15 @@ class ApplicationController < ActionController::Base
 
   #before_action :authenticate_user!, :except => [:index, :show]
   protect_from_forgery with: :exception
+  protected
 
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      #redirect_to '/users/sign_in', :notice => 'Please login first'
-      ## if you want render 404 page
-      render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-    end
+  def configure_permitted_parameters
+    # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :current_password, :date_of_birth])
+    #devise_parameter_sanitizer.for(:account_update) { |u| u.permit( :email, :password, :current_password, :date_of_birth) }
   end
+
   def after_sign_in_path_for(resource)
     case resource
       when User then
