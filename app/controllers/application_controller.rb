3662f1c56175bbf27c0c51efb7f6cdc79d48f53f
st118173 <st118173@ait.asia>
2016-11-18 14:29:31 +0700

    hj

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,28 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :current_password, :date_of_birth])
     #devise_parameter_sanitizer.for(:account_update) { |u| u.permit( :email, :password, :current_password, :date_of_birth) }
   end
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      #redirect_to '/users/sign_in', :notice => 'Please login first'
+      ## if you want render 404 page
+      render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
+  def after_sign_in_path_for(resource)
+    case resource
+      when User then
+        if @mdo.card.card_transaction.params["Ack"] == 'Success'
+          new_reg_path
+
+        else
+          root_path
+        end
+      when Admin::Admin then root_path
+    end
+  end
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
