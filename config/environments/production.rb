6f0efdb4ee8ac4fc26329d984e898275611f51a3
st118173 <st118173@ait.asia>
2016-11-25 16:25:32 +0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
-      :domain => "gmail.com",
+      :domain => "maaaapkichaaya.herokuapp.com",
       :user_name => "jntu784@gmail.com",
       :password => "endaqoligsorkrvo",
       :authentication => 'plain',
